7a1ec4d6f75557d2d112afc6e99a210b9b1b0e89
mkumar0802 <mkumar0802@gmail.com>
2022-05-22 22:47:07 +0530

    nav bar bug fix

--- src/Pages/Footer.js
+++ src/Pages/Footer.js
@@ -1,18 +1,15 @@
 import React, { useEffect } from "react";
 import SocialLinks from "../components/SocialLinks";
-import AOS from 'aos';
-import 'aos/dist/aos.css';
+
 
 function Footer() {
-    useEffect(() => {
-        AOS.init({ duration: 2000 });
-      }, [])
+    
 
     return (
         <div className="relative">
             <div   className="bg-cyan-500 text-white py-20 text-center">
                 <h2 className="text-3xl md:text-5xl mb-3">Want to connect?</h2>
-                <div data-aos="zoom-in-right"   className="my-5 md:text-xl ">
+                <div    className="my-5 md:text-xl ">
                     <p>
                         Drop me an email at<a href="mailto:https://mkumar0802@gmail.com" target="_blank" rel="noreferrer"> <span className="underline underline-offset-4">mkumar0802@gmail.com</span></a> and let's talk some more!
                     </p>
