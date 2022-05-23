be0fc5e4fb47618f4347bed51cb1f20d20934912
mkumar0802 <mkumar0802@gmail.com>
2022-05-23 10:01:37 +0530

    animate bug fix

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -46,7 +46,7 @@ function Contact() {
         <div className= "my-28 flex justify-evenly items-center flex-col scroll-mt-20 ">
             <Heading title="Hire me" />
             <div class=" flex items-top justify-center min-h-screen bg-white  sm:items-center sm:pt-0">
-                <div data-aos="flip-left" class="max-w-6xl mx-auto sm:px-6 lg:px-8">
+                <div data-aos="fade-right" class="max-w-6xl mx-auto sm:px-6 lg:px-8">
                     <div class="mt-8 overflow-hidden">
                         <div class="grid grid-cols-1 md:grid-cols-2">
                             <div class="p-6 mr-2 rounded-lg shadow-xl bg-slate-200 max-w-sm hover:bg-slate-400  sm:rounded-lg  ">
